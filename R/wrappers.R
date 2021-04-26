d5e6548fcb46ed4c5a77965420df8921fab6d768
aghaynes <aghaynes@gmail.com>
2021-04-26 13:36:11 +0200

    named arguments

--- R/wrappers.R
+++ R/wrappers.R
@@ -1,20 +1,21 @@
 # wrappers
 
 
-wrap1 <- function(data,
-                  var,
-                  by = NULL,
-                  kpi_fn = kpi_fn_mean,
-                  txt = "",
-                  cutpoints = NULL,
-                  cutlabels = NULL){
+kpi_by <- function(data,
+                   var = var,
+                   by = NULL,
+                   kpi_fn = kpi_fn_mean,
+                   txt = "",
+                   cutpoints = NULL,
+                   cutlabels = NULL){
 
+  print(by)
   out <- list()
   out$calc <- calc_kpi(data,
-                       var,
-                       by,
-                       kpi_fn,
-                       txt
+                       var = var,
+                       by = by,
+                       kpi_fn = kpi_fn,
+                       txt = txt
                        )
 
   out$outlier <- kpi_outlier(out$calc)
@@ -27,41 +28,44 @@ wrap1 <- function(data,
 
   out
 
-
 }
 
 
 
 
-wrap <- function(data,
-                 var,
-                 by = NULL,
-                 cutpoints = NULL,
-                 cutlabels = NULL
-                 ){
+kpi <- function(data,
+                   var,
+                   by = NULL,
+                   kpi_fn = kpi_fn_mean,
+                   txt = "",
+                   cutpoints = NULL,
+                   cutlabels = NULL
+                   ){
 
-
+  print("start")
   out <- list()
-  out$calc <- calc_kpi(data,
-                       var,
-                       kpi_fn = kpi_fn_mean
-                       )
-
-  out$outlier <- kpi_outlier(out$calc)
-
-
-
+  out$overall <- kpi_by(data
+                        , var = var
+                        , kpi_fn = kpi_fn
+                        , txt = txt
+                        , cutpoints = cutpoints
+                        , cutlabels = cutlabels
+                        )
+  print("by")
 
   if (!is.null(by)){
 
     for (byi in by){
-      out[[paste0("calc_", byi)]] <- calc_kpi(data,
-                                             var,
-                                             by = byi,
-                                             kpi_fn = kpi_fn_mean)
+      print(byi)
 
-      out[[paste0("outlier_", byi)]] <- kpi_outlier(out[[paste0("calc_", byi)]])
-
+      out[[byi]] <- kpi_by(data
+                           , var = var
+                           , by = byi
+                           , kpi_fn = kpi_fn
+                           , txt = txt
+                           , cutpoints = cutpoints
+                           , cutlabels = cutlabels
+                           )
 
     }
 
@@ -77,7 +81,11 @@ wrap <- function(data,
 #   mutate(cylgt4 = cyl > 4) %>%
 #   # wrap1(var = "mpg", by = c("am")) %>%
 #   # wrap1(var = "mpg", by = c("am"), cutpoints = c(0, 22, 50)) %>%
-#   wrap1(var = "mpg", cutpoints = c(0, 22, 50), txt = "MPG") %>%
+#   # wrap1(var = "mpg", cutpoints = c(0, 22, 50), txt = "MPG") %>%
+#   kpi(var = "mpg", cutpoints = c(0, 22, 50), by = c("am", "cylgt4"), txt = "MPG") %>%
+#   # kpi(var = "mpg", cutpoints = c(0, 22, 50), by = "am", txt = "MPG") #%>%
+#   # kpi_by(var = "mpg", by = "am", txt = "MPG") %>%
+#   # kpi_by(var = "mpg", txt = "MPG") %>%
 #   boomer::boom()
 
 
