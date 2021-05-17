52a3f85c005c883c6ef43bcfea9b37653d80f0f5
aghaynes <aghaynes@gmail.com>
2021-05-17 08:59:02 +0200

    extra example

--- R/theme_kpitools.R
+++ R/theme_kpitools.R
@@ -2,13 +2,17 @@
 #'
 #' Theme based on \code{theme_bw} and removing y-axis and moving the legend to beneath the plot.
 #'
-#' @return
+#' @return \code{ggplot2} theme object
 #' @export
 #' @importFrom ggplot2 theme_bw %+replace% theme element_blank margin
 #'
 #' @examples
 #' kpi <- mtcars %>%
 #'   kpi("mpg", by = "cyl", txt = "MPG")
+#'
+#' # without the theme
+#' plot(kpi)$cyl
+#' # with the theme
 #' plot(kpi)$cyl +
 #'   theme_kpitools()
 theme_kpitools <- function() {
