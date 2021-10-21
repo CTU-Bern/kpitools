22bc6ff3afa05fd84db08b7b39faa99f8aeb5ee8
Alan Haynes <aghaynes@users.noreply.github.com>
2021-10-21 15:54:27 +0200

    option to change fill

--- R/fab_dow.R
+++ R/fab_dow.R
@@ -13,7 +13,8 @@
 #' @param dow_fmt format for day of week
 #' @param output output format \code{facet} combines figures via ggplot2::facet_wrap,
 #'   \code{list} returns a list of ggplot2 plots
-#' @param col colour to use for bars
+#' @param col colour to use for bar lines
+#' @param fill colour to use for bar fill
 #' @param ... options passed to facet_wrap (see examples)
 #'
 #' @return list or ggplot2 object
@@ -32,11 +33,20 @@
 #' dat %>% fab_dow("x", "by", scales = "free_x")
 #' # different colour bars
 #' dat %>% fab_dow("x", fill = "orange")
-#' #list of plots
+#' # list of plots
 #' dat %>% fab_dow("x", "by", output = "list")
+#' # change colours
+#' dat %>% fab_dow("x", col = "purple", fill = "pink")
 #'
 
-fab_dow <- function(data, var, by = NULL, dow_fmt = "%a", output = c("facet", "list"), col = "grey", ...){
+fab_dow <- function(data,
+                    var,
+                    by = NULL,
+                    dow_fmt = "%a",
+                    output = c("facet", "list"),
+                    col = "grey",
+                    fill = "grey",
+                    ...){
 
   if(!is.character(var) | length(var) > 1) stop("'var' should be character of length 1")
   if(!is.null(by) && (!is.character(by) | length(by) > 1)) stop("'by' should be character of length 1")
@@ -61,11 +71,11 @@ fab_dow <- function(data, var, by = NULL, dow_fmt = "%a", output = c("facet", "l
     mutate(dow = as.numeric(format(.data$date, format = "%u")),
            dow = factor(dow, 7:1, rev(format(as.Date("2021-04-12") + 0:6, dow_fmt))))
 
-  fn <- function(dat, col){
+  fn <- function(dat, col, fill){
     dat %>%
       filter(!is.na(dow)) %>%
       ggplot(aes(x = dow)) +
-      geom_bar(show.legend = FALSE, col = col) +
+      geom_bar(show.legend = FALSE, col = col, fill = fill) +
       scale_fill_discrete(drop = FALSE) +
       scale_x_discrete(drop = FALSE) +
       coord_flip() +
@@ -75,15 +85,15 @@ fab_dow <- function(data, var, by = NULL, dow_fmt = "%a", output = c("facet", "l
 
   if(length(unique(dat$by)) > 1){
     if(output == "facet"){
-      out <- fn(dat, col) +
+      out <- fn(dat, col, fill) +
         facet_wrap(~ by, ...)
     }
     if(output == "list"){
       dat <- split(dat, dat$by)
-      out <- map(dat, fn, col)
+      out <- map(dat, fn, col = col, fill = fill)
     }
   } else {
-    out <- fn(dat, col)
+    out <- fn(dat, col, fill)
   }
 
   return(out)
